da65dc7a1d9bb00edfb8f5e777ac5db720d41ff2
csirirathasuk14 <csirirathasuk14@gmail.com>
2016-09-19 18:20:29 -0400

    first commit

---
+++ DictionariesSlide22/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionariesSlide22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> animals = new Dictionary<string, int>();
+            animals.Add("Bear", 4);
+            animals.Add("Squid", 10);
+            animals.Add("Bird", 2);
+            animals.Add("Cat", 5);
+            animals.Add("Dog", 8);
+            animals.Add("Horse", 7);
+
+            if(animals.ContainsKey("Bear"))
+            {
+                int value = animals["Bear"];
+                Console.WriteLine("Bear has " + value + " legs.");
+            }
+
+            if (animals.ContainsKey("Chicken"))
+            {
+                int value = animals["Chicken"];
+                Console.WriteLine("Bear has " + value + " legs.");
+            }
+
+
+        }
+    }
+}
